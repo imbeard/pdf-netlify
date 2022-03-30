3608ed9725aa422d487fca386665dba81a2285b2
Andrea <andrea@imbeard.com>
2022-03-30 14:42:05 +0200

    chromium

--- functions/pdf.js
+++ functions/pdf.js
@@ -14,7 +14,7 @@ function isFullUrl(url) {
 }
 
 async function saveToPdf(url) {
-    const browser = await puppeteer.launch({ args: ["--no-sandbox", "--disable-setuid-sandbox"] });
+    const browser = await chromium.puppeteer.launch({ args: ["--no-sandbox", "--disable-setuid-sandbox"] });
     const page = await browser.newPage();
     await page.goto(url);
     const pdf = await page.pdf({ format: 'a4', scale: 0.5, printBackground: true });
