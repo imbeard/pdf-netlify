de991a43a2babb4e761b714644c227e543ae2f5f
Andrea Facheris <imbeard@users.noreply.github.com>
2023-10-26 18:03:45 +0200

    Update pdf.js

--- functions/pdf.js
+++ functions/pdf.js
@@ -31,7 +31,7 @@ exports.handler = async (event, context) => {
     const browser = await puppeteer.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
+      executablePath: await chromium.executablePath(),
       headless: true, //chromium.headless,
     })
     
